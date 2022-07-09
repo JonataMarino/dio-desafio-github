60867090dad8ea4be79e374bc854e4d0df131531
JonataMarino <jsmarino1993@outlook.com>
2022-07-09 13:41:21 -0300

    M II - Fundamentos web

---
+++ Modulo II - Fundamentos Web com HTML e CSS/Aula prática/projetos/index.php
@@ -0,0 +1,15 @@
+<html>
+
+		<head>
+			<title> Meu Primeiro Site em PHP! Woohoo!</title>
+		</head>
+		
+		<body>
+			<?php
+				for ( $i = 0; $i < 10; $i++) {
+					print("Linha Número" .$i+1 . "<br />");
+				}
+			?>
+		</body>
+		
+</html>
