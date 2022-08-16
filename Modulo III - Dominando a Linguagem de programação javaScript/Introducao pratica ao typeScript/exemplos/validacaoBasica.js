34c6cd402e28fd7242a7cf72e512fc7cd88a9236
JonataMarino <jsmarino1993@outlook.com>
2022-08-16 12:34:08 -0300

    Introducao Pratica ao TypeScript

---
+++ Modulo III - Dominando a Linguagem de programação javaScript/Introducao pratica ao typeScript/exemplos/validacaoBasica.js
@@ -0,0 +1,22 @@
+/* let button = document.getElementById('button')
+let input1 = document.getElementById('input1')
+let input2 = document.getElementById('input2')
+
+function somarNumeros (numero1, numero2){
+    if( typeof numero1 === 'number' && typeof numero2 === 'number'){
+        return numero1 + numero2;
+    }else{
+        return Number(numero1) + Number(numero2)    
+    } 
+}
+
+
+button.addEventListener('click', ()=> {
+    console.log(somarNumeros(input1.value, input2.value));
+}) */
+
+/*
+$ npm install typescript --save-dev //As dev dependency.
+$ npm install typescript -g //Install as a global module.
+$ npm install typescript@latest -g //Install latest if you have an older version.
+*/
